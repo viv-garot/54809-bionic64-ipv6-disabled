32cdeea72bc439b4366797bcf6acd790c90e9c3e
viv-garot <vivien@hashicorp.com>
2021-08-30 17:20:25 +0200

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -10,11 +10,12 @@ apt-get install -y linux-generic linux-image-generic
 # basic tools
 apt-get install -y  \
 	git vim curl wget jq tar unzip software-properties-common net-tools \
-	language-pack-en ctop htop sysstat nginx
+	language-pack-en ctop htop sysstat 
 
 # Hide Ubuntu splash screen during OS Boot, so you can see if the boot hangs
 apt-get remove -y plymouth-theme-ubuntu-text
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT=""/' /etc/default/grub
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="ipv6.disable=1"/' /etc/default/grub
+sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="ipv6.disable=1"/' /etc/default/grub
 update-grub
 
 # Reboot with the new kernel
